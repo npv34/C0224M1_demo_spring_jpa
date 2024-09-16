6355dd0a14ca498315397abe600761696bc40ab0
npv34 <phanvanluan611996@gmail.com>
2024-09-16 21:16:43 +0700

    demo jpa

--- src/main/java/org/codegym/demomvc/controller/GroupController.java
+++ src/main/java/org/codegym/demomvc/controller/GroupController.java
@@ -4,20 +4,51 @@ import org.codegym.demomvc.model.Group;
 import org.codegym.demomvc.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/groups")
 public class GroupController {
-    @Autowired
+
     private GroupService groupService;
 
-    @RequestMapping("")
-    public String index() {
+    @Autowired
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+    @GetMapping("")
+    public String groupList(Model model) {
         List<Group> groups = groupService.getAllGroups();
         System.out.println(groups.size());
+        model.addAttribute("groups", groups);
         return "groups/list";
+    }
+
+    @GetMapping("/{id}/update")
+    public String updateGroup(@PathVariable("id") int id, Model model) {
+        Group groupUpdate = groupService.findById(id);
+        model.addAttribute("group", groupUpdate);
+        return "groups/update";
+    }
+
+    @PostMapping("/{id}/update")
+    public String updateGroup(@PathVariable("id") int id, @RequestParam String name) {
+        Group groupUpdate = groupService.findById(id);
+        groupService.updateGroup(groupUpdate, name);
+        return "redirect:/groups";
+    }
+
+    @GetMapping("/create")
+    public String createGroup() {
+        return "groups/create";
+    }
+
+    @PostMapping("/create")
+    public String createGroup(@RequestParam String name) {
+        groupService.createGroup(name);
+        return "redirect:/groups";
     }
 }
--- src/main/java/org/codegym/demomvc/service/GroupService.java
+++ src/main/java/org/codegym/demomvc/service/GroupService.java
@@ -10,10 +10,29 @@ import java.util.List;
 @Service
 public class GroupService {
 
-    @Autowired
     private GroupRepository groupRepository;
+
+    @Autowired
+    public GroupService(GroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
 
     public List<Group> getAllGroups() {
         return groupRepository.findAll();
+    }
+
+    public Group findById(long id) {
+        return groupRepository.findById(id).orElse(null);
+    }
+
+    public void updateGroup(Group group, String name) {
+        group.setName(name);
+        groupRepository.save(group);
+    }
+
+    public void createGroup(String name) {
+        Group group = new Group();
+        group.setName(name);
+        groupRepository.save(group);
     }
 }
